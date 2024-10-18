a7adb3b2f636fbe88b3491583d4b3967c3af2650
ozgurd5 <ozgurdalbeler@gmail.com>
2024-10-18 16:37:37 +0300

    More Data

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 import pandas as pd
 import time
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
 # WebDriver başlatılıyor - Chrome tarayıcı
 print("Tarayıcı başlatılıyor...")
 driver = webdriver.Chrome()
@@ -10,13 +13,17 @@ driver = webdriver.Chrome()
 # İlanları tutmak için bir liste
 ilan_listesi = []
 
-# 20 sayfa boyunca gezinme işlemi
-for page in range(1, 21):
+# İlanların linklerini tutmak için bir liste. Eşya ve doğalgaz durumlarına buradan ulaşacağız
+ilan_linkleri = []
+
+# 22 sayfa boyunca gezinme işlemi
+for page in range(1, 22):
     # Sayfa URL'si oluşturuluyor ve siteye gidiliyor
     url = f'https://www.hepsiemlak.com/buca-kiralik?page={page}'
     print(f"Sayfa {page} yükleniyor: {url}")
     driver.get(url)
-    time.sleep(5)  # Sayfanın tamamen yüklenmesi için 5 saniye bekleniyor, süre uzatılabilir
+    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'listing-item')))
+    print("Sayfa yüklendi.")
 
     # İlan kartlarını bulma
     ilanlar = driver.find_elements(By.CLASS_NAME, 'listing-item')
@@ -50,6 +57,10 @@ for page in range(1, 21):
             # İlan tarihi çıkarılıyor
             ilan_tarihi = ilan.find_element(By.CLASS_NAME, 'list-view-date').text
 
+            # İlan linkini almak ve tam URL'yi oluşturmak
+            ilan_linki = ilan.find_element(By.CLASS_NAME, 'card-link').get_attribute('href')
+            ilan_linkleri.append(ilan_linki)
+
             # İlan verileri ilana ekleniyor
             ilan_listesi.append({
                 'Fiyat': fiyat,
@@ -59,13 +70,59 @@ for page in range(1, 21):
                 'Alan': alan,
                 'Bina Yaşı': bina_yasi,
                 'Kat': kat_sayisi,
-                'İlan Tarihi': ilan_tarihi
+                'İlan Tarihi': ilan_tarihi,
+                'İlan Linki': ilan_linki,
             })
             print(f"İlan eklendi: {baslik}")
+            print(f"İlan linki eklendi: {ilan_linki}")
+
         except Exception as e:
             # Hata durumunda mesaj yazdırılıyor, işleme bir sonraki sayfadan devam ediliyor
             print(f'Error: {e}')
             continue
+
+for ilan_linki in ilan_linkleri:
+    print("İlan linki yükleniyor: ", ilan_linki)
+    driver.get(ilan_linki)
+    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, 'txt')))
+    print("İlan linki yüklendi.")
+
+    try:
+        # Tüm span'ları bul
+        spans = driver.find_elements(By.CLASS_NAME, 'txt')
+
+        esya_durumu = ''
+        yakit_tipi = ''
+        isinma_tipi = ''
+
+        for span in spans:
+            if span.text == 'Eşya Durumu':  # Eğer span 'Eşya Durumu' metnini içeriyorsa
+                # Kardeş öğesi olan diğer span'dan Eşya Durumu'nu al
+                esya_durumu = span.find_element(By.XPATH, "following-sibling::span").text
+
+            elif span.text == 'Yakıt Tipi':  # Eğer span 'Yakıt Tipi' metnini içeriyorsa
+                # Kardeş öğesi olan diğer span'dan Yakıt Tipi'ni al
+                yakit_tipi = span.find_element(By.XPATH, "following-sibling::span").text
+
+            elif span.text == 'Isınma Tipi':  # Eğer span 'Isınma Tipi' metnini içeriyorsa
+                # Kardeş öğesi olan diğer span'dan Isınma Tipi'ni al
+                isinma_tipi = span.find_element(By.XPATH, "following-sibling::span").text
+
+            if esya_durumu and yakit_tipi and isinma_tipi != '':  # Eğer tüm bilgiler alındıysa
+                break # Döngüden çık
+
+        # İlan listesine 'Eşya Durumu', 'Yakıt Tipi' ve 'Isınma Tipi' ekleniyor
+        ilan_index = ilan_linkleri.index(ilan_linki)
+        ilan_listesi[ilan_index]['Eşya Durumu'] = esya_durumu
+        ilan_listesi[ilan_index]['Yakıt Tipi'] = yakit_tipi
+        ilan_listesi[ilan_index]['Isınma Tipi'] = isinma_tipi
+
+        print(f"İlan verileri eklendi: {esya_durumu}, {yakit_tipi}, {isinma_tipi}")
+
+    except Exception as e:
+        print(f'Error: {e}')
+        continue
+
 
 print("Tarayıcı kapatılıyor...")
 driver.quit()
